f3124dce40ebe39768fda28b0845f12e27f9a701
Artem Sydorov <sydorovay@gmail.com>
2023-05-22 00:14:45 +0200

    add user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import Profile from './Profile';
+import user from './json/User.json'
+
+const App = () => (
+  <div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  </div>
+);
+
+export default App;
