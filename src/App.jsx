a45f1c79154574db3dff36fd54fb20f83ca897df
Artem Sydorov <sydorovay@gmail.com>
2023-05-22 23:27:46 +0200

    edit tree elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Profile from './Profile';
+import Profile from './components/profile/Profile';
 import user from './json/User.json';
-import Statistics from './Statistics';
+import Statistics from './components/statistics/Statistics';
 import data from './json/data.json';
-import FriendList from './FriendList';
+import FriendList from './components/friend-list/FriendList';
 import friends from './json/friends.json';
 import transactions from './json/transactions.json';
-import TransactionHistory from './Transactions';
+import TransactionHistory from './components/transactions/Transactions';
 
 const App = () => (
   <div>
