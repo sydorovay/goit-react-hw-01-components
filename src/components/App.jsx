315adf303e006eb25bd972fda31b7dfe7ce3356f
Artem Sydorov <sydorovay@gmail.com>
2023-05-22 23:02:32 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,11 @@
-import React from 'react';
 import Profile from './Profile';
 import user from './json/User.json';
 import Statistics from './Statistics';
 import data from './json/data.json';
 import FriendList from './FriendList';
-import friends from './json/friends.json'; 
+import friends from './json/friends.json';
 import transactions from './json/transactions.json';
 import TransactionHistory from './Transactions';
-
 
 const App = () => (
   <div>
@@ -19,8 +17,8 @@ const App = () => (
       stats={user.stats}
     />
     <Statistics title="Upload stats" stats={data} />
-    <FriendList friends={friends} />;
-    <TransactionHistory items={transactions} />;
+    <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />
   </div>
 );
 
